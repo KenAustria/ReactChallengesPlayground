a577578e092aa1885b377c5f2cf67d55c0ef9598
Ken Austria <kenaustriadev@gmail.com>
2020-09-16 20:38:06 -0700

    LimitedTextarea styled and tested getByTestId fireEvent.change

--- src/components/LimitedTextarea/LimitedTextarea.js
+++ src/components/LimitedTextarea/LimitedTextarea.js
@@ -1,30 +1,112 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-const LimitedTextarea = ({rows, cols, value, limit}) => {
-	const [content, setContent] = useState(value)
+const Textarea = styled.textarea`
+  width: 400px;
+  height: 150px;
+  border: 2px solid #808080;
+  border-radius: 10px;
+  padding: 7px;
+  margin: 7px;
+  font-family: Arial, sans-serif;
+`;
 
-	// trims the content of the input if it's longer than limit
-	const setFormattedContent = (text) => {
-		text.length > limit ? setContent(text.splice(0, limit)) : setContent(text)
-	}
+const LimitedTextarea = ({ value = "Cheers! 🍻", limit = "20" }) => {
+  const [content, setContent] = useState(value);
 
-	useEffect(() => {
-		setFormattedContent(content)
-	}, [])
+  const setFormattedContent = text => {
+    text.length > limit ? setContent(text.slice(0, limit)) : setContent(text);
+  };
 
-	return (
-		<div>
-			<textarea
-				rows={rows}
-				cols={cols}
-				value={content}
-				onChange={event => setFormattedContent(event.target.value)}
-			/>
-			<p>{content.length} / {limit}</p>
-		</div>
-	)
-}
+  useEffect(() => {
+    setFormattedContent(content);
+  }, []);
+
+  return (
+    <div>
+      <Textarea
+        value={content}
+        placeholder="10 Character Limit"
+        data-testid="textarea"
+        onChange={event => setFormattedContent(event.target.value)}
+      />
+      <p>
+        {content.length}/{limit}
+      </p>
+    </div>
+  );
+};
+
+LimitedTextarea.propTypes = {
+  limit: PropTypes.number,
+  value: PropTypes.string
+};
 
 export default LimitedTextarea;
 
-{/* <LimitedTextarea value='supdawg' limit={50} /> */}
+// 1st Attempt
+// const LimitedTextarea = ({ limit }) => {
+//   const [text, setText] = useState("");
+
+//   return (
+//     <form>
+// 			<label>Handle</label>
+//       <input type="text" name="handle" value="text" onChange={event => setText(event.target.value)} />
+//     </form>
+//   );
+// };
+
+// 2nd Attempt
+// const LimitedTextarea = ({ limit }) => {
+// const [text, setText] = useState({ handle: "", location: ""});
+
+//   const inputChangeHandler = event => {
+//     const value = event.target.value;
+//     // extract value from event object and assign to dynamic key
+//     setText({ ...text, [event.target.name]: value });
+//   };
+
+//   return (
+//     <form>
+//       <label>Handle</label>
+//       <input type="text" name="handle" value={text.handle} onChange={inputChangeHandler} />
+//       <label>Location</label>
+//       <input type="text" name="location" value={text.location} onChange={inputChangeHandler}/>
+//     </form>
+//   );
+// };
+
+// 3rd Attempt
+// const LimitedTextarea = ({ limit }) => {
+//   const [text, setText] = useState({
+//     handle: "",
+//     location: ""
+//   });
+
+//   const inputChangeHandler = event => {
+//     const value = event.target.value;
+//     if (value < limit) {
+//       setText({ ...text, [event.target.name]: value });
+//     }
+//   };
+
+//   return (
+//     <form>
+//       <label>Handle</label>
+//       <input
+//         type="text"
+//         name="handle"
+//         value={text.handle}
+//         onChange={inputChangeHandler}
+//       />
+//       <label>Location</label>
+//       <input
+//         type="text"
+//         name="location"
+//         value={text.location}
+//         onChange={inputChangeHandler}
+//       />
+//     </form>
+//   );
+// };
