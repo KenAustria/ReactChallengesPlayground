26f05a20a9a8969b3ca71f3b34f6a3b8fb0a2f17
Ken Austria <kenaustriadev@gmail.com>
2020-10-17 13:58:23 -0700

    Slideshow useReducer

--- src/components/Slideshow/src/App.js
+++ src/components/Slideshow/src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import './App.css';
 import 'h8k-components';
-
-import Slides from './components/Slides';
+import { v4 as uuid } from 'uuid';
+import Slides from './components/Slides'
 const title = "Slideshow App";
 
-function App({slides}) {
+const SLIDES = [
+	{
+		id: uuid(),
+		title: "Today's workout plan",
+		text: "We're gonna do 3 fundamental exercises."
+	},
+	{
+		id: uuid(),
+		title: "First, 10 push-ups",
+		text: "Do 10 reps. Remember about full range of motion. Don't rush."
+	},
+	{
+		id: uuid(),
+		title: "Next, 20 squats",
+		text: "Squats are important. Remember to keep your back straight."
+	},
+	{
+		id: uuid(),
+		title: "Finally, 15 sit-ups",
+		text: "Slightly bend your knees. Remember about full range of motion."
+	},
+	{
+		id: uuid(),
+		title: "Great job!",
+		text: "You made it, have a nice day and see you next time!"
+	}
+];
+
+function App() {
 	return (
 		<div>
 			<h8k-navbar header={title}></h8k-navbar>
 			<div className="App">
-				<Slides slides={slides} />
+				<Slides slides={SLIDES} />
 			</div>
 		</div>
 	);
--- src/components/Slideshow/src/components/Slides.js
+++ src/components/Slideshow/src/components/Slides.js
@@ -1,34 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react'
+import slideshowReducer from '../slideshowReducer'
 
-function Slides({slides}) {
-	const [currSlide, setCurrSlide] = useState(0)
+const initialState = { currSlide: 0}
 
-	useEffect(() => {
-    setCurrSlide(currSlide)
-  }, [currSlide]);
-
-	const nextSlideHandler = () => {
-		setCurrSlide(currSlide + 1)
-	}
-
-	const prevSlideHandler = () => {
-		setCurrSlide(currSlide - 1)
-	}
+const Slides = ({slides}) => {
+	const [state, dispatchSlideshow] = useReducer(slideshowReducer, initialState)
 
 	const restartHandler = () => {
-		setCurrSlide(0)
+		dispatchSlideshow({ type: 'RESTART'})
+	}
+
+	const prevHandler = () => {
+		dispatchSlideshow({ type: 'PREV'})
+	}
+
+	const nextHandler = () => {
+		dispatchSlideshow({ type: 'NEXT'})
 	}
 
 	return (
 		<div>
 			<div id="navigation" className="text-center">
-				<button data-testid="button-restart" className="small outlined" disabled={currSlide === 0} onClick={restartHandler}>Restart</button>
-				<button data-testid="button-prev" className="small" disabled={currSlide === 0} onClick={prevSlideHandler}>Prev</button>
-				<button data-testid="button-next" className="small" disabled={currSlide === slides.length - 1}  onClick={nextSlideHandler}>Next</button>
+				<button
+					data-testid="button-restart"
+					className="small outlined"
+					disabled={state.currSlide === 0}
+					onClick={restartHandler}
+					>
+						Restart
+				</button>
+				<button
+					data-testid="button-prev"
+					className="small"
+					disabled={state.currSlide === 0}
+					onClick={prevHandler}
+					>
+						Prev
+				</button>
+				<button
+					data-testid="button-next"
+					className="small"
+					disabled={state.currSlide === slides.length - 1}
+					onClick={nextHandler}
+					>
+						Next
+				</button>
 			</div>
 			<div id="slide" className="card text-center">
-				<h1 data-testid="title">{slides[currSlide].title}</h1>
-				<p data-testid="text">{slides[currSlide].text}</p>
+			<h1 data-testid="title">{slides[state.currSlide].title}</h1>
+			<p data-testid="text">{slides[state.currSlide].text}</p>
 			</div>
 		</div>
 	);
