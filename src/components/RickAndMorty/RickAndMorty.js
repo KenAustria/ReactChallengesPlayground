1aa25d92fb308d05b5ede2fb0008edbee05a6f60
Ken Austria <kenaustriadev@gmail.com>
2021-04-10 21:16:08 -0700

    Redisplay individual cache

--- src/components/RickAndMorty/RickAndMorty.js
+++ src/components/RickAndMorty/RickAndMorty.js
@@ -39,94 +39,11 @@ export default App;
 
 
 import React from "react";
-import SearchForm from "../SearchForm"
+import SearchForm from "../SearchForm";
 import RickAndMortyInfo from "../RickAndMortyInfo";
-import Cache from "../Cache"
+import Cache from "../Cache";
 import axios from "axios";
 import { useQuery, useQueryClient } from "react-query";
-import PropTypes from "prop-types"
-
-const Home = ({ idQuery, setIdQuery}) => {
-  const queryClient = useQueryClient();
-  const characterData = queryClient.getQueryData(["rickandmorty", idQuery])
-
-	const cacheData = queryClient.getQueryCache(["rickandmorty", idQuery], {
-		exact: false,
-		enabled: false
-	})
-
-  const cachedCharacters = Object.values(
-    cacheData.queriesMap
-  ).map((character, index) => (
-    <>
-      {character.state.data ? (
-        <button key={`${character.state.data.name}${index}`}>
-          {character.state.data.name}
-        </button>
-      ) : null}
-    </>
-  ));
-
-  const delay = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 500);
-    });
-  }
-
-	const handleRickAndMortyFetch = () => {
-    return delay()
-      .then(() => axios(`https://rickandmortyapi.com/api/character/${idQuery}`))
-			.then((res) => res.data);
-  };
-
-	const { data, error, isLoading } = useQuery(
-    ["rickandmorty", idQuery],
-    handleRickAndMortyFetch,
-    {
-			cacheTime: 1000 * 60 * 60 * 24,
-      enabled: false || idQuery !== 0,
-      onError: (error) => console.log(error),
-      retry: false,
-      refetchOnWindowFocus: true,
-      staleTime: 1000 * 60 * 60 * 24,
-      useErrorBoundary: true
-    }
-  );
-
-  const handleFetchOnInputChange = event => {
-    setIdQuery(event.target.value, () => {
-      handleRickAndMortyFetch()
-    })
-	}
-
-  return (
-    <div>
-      <SearchForm
-        error={error}
-        cacheData={cacheData}
-        characterData={characterData}
-        handleFetchOnInputChange={handleFetchOnInputChange}
-        idQuery={idQuery}
-        isLoading={isLoading}
-        queryClient={queryClient}
-        setIdQuery={setIdQuery}
-      />
-      <RickAndMortyInfo rickAndMortyCharacter={data} />
-      <Cache cachedCharacters={cachedCharacters} />
-    </div>
-  )
-}
-
-Home.propTypes = {
-  setIdQuery: PropTypes.func
-}
-
-export default Home
-
-
-import React from "react";
 import PropTypes from "prop-types";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -136,19 +53,131 @@ const searchSchema = yup.object().shape({
   rickAndMortyId: yup.number().required().positive().integer()
 });
 
-const SearchForm = ({ cacheData, cachedCharacters, error, handleFetchOnInputChange, idQuery, isLoading, queryClient, refetch, setIdQuery }) => {
-  const { register, handleSubmit, errors, setValue, watch } = useForm({
+const Home = ({ idQuery, setIdQuery }) => {
+  const queryClient = useQueryClient();
+  // check if data exist in cache
+  const characterData = queryClient.getQueryData(["rickandmorty", idQuery]);
+  // ternary result to be used on 'enabled'
+  const charBool = characterData ? true : false;
+
+  const { errors, handleSubmit, register, reset, setValue, watch } = useForm({
     resolver: yupResolver(searchSchema)
   });
+
+  const handleSingleLogCache = () => {
+    console.log(characterData) // returns data
+    console.log(charBool) // returns true
+  }
+
+  const cacheData = queryClient.getQueryCache(["rickandmorty", idQuery], {
+    exact: false,
+    enabled: false
+  });
+
+  const handleRickAndMortyFetch = () => {
+    return delay()
+      .then(() => axios(`https://rickandmortyapi.com/api/character/${idQuery}`))
+      .then((res) => res.data)
+      .then(
+        console.log(characterData), // returns undefined
+        console.log(charBool) // returns false
+      );
+  };
+
+  const cachedCharacters = Object.values(
+    cacheData.queriesMap
+    ).map((character, index) => (
+      <>
+        {character.state.data ? (
+          <button
+            key={`${character.state.data.name}${index}`}
+            style={{ width: "3%", height: "10%"}}
+            onClick={() => setIdQuery(character.state.data.id)}
+          >
+            <img src={character.state.data.image} alt="character" style={{ width: "85%", height: "15%" }} />
+          </button>
+        ) : null}
+      </>
+    )
+  );
+
+  const delay = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 500);
+    });
+  };
+
+  const { data, error, isLoading } = useQuery(
+    ["rickandmorty", idQuery],
+    handleRickAndMortyFetch,
+    {
+      cacheTime: 1000 * 60 * 60 * 24,
+      enabled: false || idQuery !== 0,
+      onError: (error) => console.log(error),
+      retry: false,
+      refetchOnWindowFocus: true,
+      staleTime: 1000 * 60 * 60 * 24,
+      useErrorBoundary: true
+    }
+  );
+
+  return (
+    <div>
+      <SearchForm
+        errors={errors}
+        handleSubmit={handleSubmit}
+        register={register}
+        reset={reset}
+        setValue={setValue}
+        watch={watch}
+        error={error}
+        cacheData={cacheData}
+        characterData={characterData}
+        idQuery={idQuery}
+        isLoading={isLoading}
+        setIdQuery={setIdQuery}
+      />
+      <button onClick={handleSingleLogCache}>Single Cache Log</button>
+      <RickAndMortyInfo rickAndMortyCharacter={data} />
+      <Cache
+        idQuery={idQuery}
+        queryClient={queryClient}
+        rickAndMortyCharacter={data}
+        cachedCharacters={cachedCharacters}
+        reset={reset}
+      />
+    </div>
+  );
+};
+
+Home.propTypes = {
+  setIdQuery: PropTypes.func
+};
+
+export default Home;
+
+
+import React from "react";
+import PropTypes from "prop-types";
+
+const SearchForm = ({
+  errors, handleSubmit, register, setValue, watch,
+  cacheData,
+  error,
+  idQuery,
+  isLoading,
+  setIdQuery
+}) => {
   const formId = watch("rickAndMortyId");
-  const disable = isLoading || parseFloat(formId) === idQuery || !idQuery || cachedCharacters;
+  const disable = isLoading || parseFloat(formId) === idQuery
 
   const handleSubmitForm = (formData) => setIdQuery(formData.rickAndMortyId);
-  const handleCacheClear = () => queryClient.removeQueries("rickandmorty")
   const handleLogCache = () => {
-		console.log(cacheData.queriesMap)
-		console.log(Object.values(cacheData.queriesMap))
-	}
+    console.log(cacheData.queriesMap);
+    console.log(Object.values(cacheData.queriesMap));
+  };
 
   const getRandomRickAndMortyCharacterId = () => {
     const NUMBER_OF_RICK_AND_MORTY_CHARACTERS = 672;
@@ -167,7 +196,6 @@ const SearchForm = ({ cacheData, cachedCharacters, error, handleFetchOnInputChan
           placeholder="Between 1 and 671"
           ref={register}
           disabled={isLoading}
-          onChange={handleFetchOnInputChange}
         />
         {errors.rickAndMortyId && <span>This field is required</span>}
         {error && <p>Error occurred: {error.message}</p>}
@@ -185,19 +213,18 @@ const SearchForm = ({ cacheData, cachedCharacters, error, handleFetchOnInputChan
         Random
       </button>
       <button onClick={handleLogCache}>Log cache from last request</button>
-      <button onClick={handleCacheClear}>Clear Cache</button>
     </div>
-  )
-}
+  );
+};
 
 SearchForm.propTypes = {
   setIdQuery: PropTypes.func,
   handleRickAndMortyFetch: PropTypes.func,
   isLoading: PropTypes.bool,
   error: PropTypes.object
-}
+};
 
-export default SearchForm
+export default SearchForm;
 
 
 import React from "react";
@@ -227,10 +254,27 @@ export default RickandMortyInfo;
 
 import React from 'react'
 
-const Cache = ({ cachedCharacters }) => {
+const Cache = ({ idQuery, queryClient, reset, rickAndMortyCharacter, cachedCharacters }) => {
+  const handleCacheClear = () => {
+    queryClient.removeQueries("rickandmorty")
+    reset()
+  };
+
+  const handleRemoveFromCache = () => queryClient.removeQueries(["rickandmorty", idQuery]);
+
   return (
     <div>
-			{cachedCharacters}
+      <div>
+        {rickAndMortyCharacter?.name ? (
+          <button onClick={handleRemoveFromCache}>Remove {rickAndMortyCharacter.name} from cache?</button>
+        ) : (
+          ""
+        )}
+        <button onClick={handleCacheClear}>Clear Cache</button>
+      </div>
+      <div>
+        {cachedCharacters}
+      </div>
 		</div>
   )
 }
