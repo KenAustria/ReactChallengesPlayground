7e5f7a3c287747e73ace5052cd65d2b04beb8983
Ken Austria <kenaustriadev@gmail.com>
2023-01-21 15:06:19 +0900

    Tabs aria roles and states

--- src/components/Tabs_GFE/App.js
+++ src/components/Tabs_GFE/App.js
@@ -5,19 +5,19 @@ const API = [
   {
     value: "html",
     label: "HTML",
-    content:
+    panel:
       "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser."
   },
   {
     value: "css",
     label: "CSS",
-    content:
+    panel:
       "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML."
   },
   {
     value: "javascript",
     label: "JAVASCRIPT",
-    content:
+    panel:
       "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS."
   }
 ];
--- src/components/Tabs_GFE/Tabs.js
+++ src/components/Tabs_GFE/Tabs.js
@@ -1,19 +1,35 @@
-import { useState } from "react";
+import { useId, useState } from "react";
+
+// declaring the function outside of scope for readability and reusability.
+
+// generates a unique id string to use as the value of the id attribute of tab items
+function getTabListItemId(tabsId, value) {
+  return tabsId + "-tab-" + value;
+}
+
+// generates unique id string to use as value of the id attribute of tabpanels
+function getTabPanelId(tabsId, value) {
+  return tabsId + "-tabpanel-" + value;
+}
 
 export default function Tabs({ defaultValue, items }) {
+  // generate unique id
+  const [tabsId] = useId();
   // if defaultValue isn't provided, use first item as value
   const [tabValue, setTabValue] = useState(defaultValue ?? items[0].value);
 
   return (
     <div className="tabs">
-      <div className="tabs-list">
+      <div className="tabs-list" role="tablist">
         {items.map(({ value, label }) => {
           // if tabValue state matches value key, then that's the active tab
           const isActiveValue = value === tabValue;
           return (
             <button
               type="button"
+              role="tab"
               key={value}
+              id={getTabListItemId(tabsId, value)}
               // tabs-list-item style is always applied
               // if isActiveValue is truthy, also apply tabs-list-item--active styles
               // elements that pass null check are passed to new array
@@ -25,6 +41,10 @@ export default function Tabs({ defaultValue, items }) {
                 .filter(Boolean)
                 .join(" ")}
               onClick={() => setTabValue(value)}
+              // identifies element whose contents are controlled by element this attribute is set
+              aria-controls={getTabPanelId(tabsId, value)}
+              // indicates current selected state
+              aria-selected={isActiveValue}
             >
               {label}
             </button>
@@ -32,10 +52,17 @@ export default function Tabs({ defaultValue, items }) {
         })}
       </div>
       <div>
-        {items.map(({ value, content }) => {
+        {items.map(({ value, panel }) => {
           return (
-            <div key={value} hidden={value !== tabValue}>
-              {content}
+            <div
+              role="tabpanel"
+              key={value}
+              id={getTabPanelId(tabsId, value)}
+              //  identifies element that labels the element it is applied to
+              aria-labelledby={getTabListItemId(tabsId, value)}
+              hidden={value !== tabValue}
+            >
+              {panel}
             </div>
           );
         })}
