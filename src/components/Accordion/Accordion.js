d5416e0b09c4c2093f0c7ecec8fb0dfb747de73c
Ken Austria <kenaustriadev@gmail.com>
2023-01-23 17:57:32 +0900

    Accordion accessibility

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,31 +1,62 @@
-import "./styles.css";
-import Accordion from "./Accordion/Accordion";
+import React, { useId, useState } from "react";
 
-const API = [
-	{
-		value: "html",
-		title: "HTML",
-		content:
-			"The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser."
-	},
-	{
-		value: "css",
-		title: "CSS",
-		content:
-			"Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML."
-	},
-	{
-		value: "javascript",
-		title: "JAVASCRIPT",
-		content:
-			"JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS."
-	}
-];
+function getAccordionHeaderId(accordionId, value) {
+  return accordionId + "-header-" + value;
+}
 
-export default function App() {
-	return (
-		<div className="wrapper">
-			<Accordion sections={API} />
-		</div>
-	);
+function getAccordionPanelId(accordionId, value) {
+  return accordionId + "-panel-" + value;
+}
+
+export default function AccordionItem({ sections }) {
+  const [accordionId] = useId();
+  const [openSections, setOpenSections] = useState(new Set());
+
+  return (
+    <div className="accordion">
+      {sections.map(({ value, title, content }) => {
+        const isExpanded = openSections.has(value);
+        const headerId = getAccordionHeaderId(accordionId, value);
+        const panelId = getAccordionPanelId(accordionId, value);
+        return (
+          <div className="accordion-item" key={value}>
+            <button
+              aria-controls={panelId}
+              aria-expanded={isExpanded}
+              className="accordion-item-title"
+              id={headerId}
+              onClick={() => {
+                const newOpenSections = new Set(openSections);
+                newOpenSections.has(value)
+                  ? newOpenSections.delete(value)
+                  : newOpenSections.add(value);
+                setOpenSections(newOpenSections);
+              }}
+              type="button"
+            >
+              {title}{" "}
+              <span
+                aria-hidden={true}
+                className={[
+                  "accordion-icon",
+                  isExpanded && "accordion-icon--rotated"
+                ]
+                  .filter(Boolean)
+                  .join(" ")}
+              />
+            </button>
+            <div
+              aria-labelledby={headerId}
+              hidden={!isExpanded}
+              id={panelId}
+              className="accordion-item-contents"
+              role="region"
+            >
+              {content}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
